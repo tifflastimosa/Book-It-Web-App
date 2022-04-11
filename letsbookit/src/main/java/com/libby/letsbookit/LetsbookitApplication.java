25d3028fd81f3bcb16e445407712dbb46ff870bf
Tiffany <lastimosa.ti@northeastern.edu>
2022-04-11 16:43:53 -0700

    add letsbookit

---
+++ letsbookit/src/main/java/com/libby/letsbookit/LetsbookitApplication.java
@@ -0,0 +1,13 @@
+package com.libby.letsbookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsbookitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsbookitApplication.class, args);
+	}
+
+}
---
+++ letsbookit/src/test/java/com/libby/letsbookit/LetsbookitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libby.letsbookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsbookitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
