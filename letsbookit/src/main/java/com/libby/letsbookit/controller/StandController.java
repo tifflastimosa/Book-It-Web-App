ff3fe444ad1a848184d100c8b181ef8f4ba4d955
moranandrew <moran.an@northeastern.edu>
2022-04-17 19:05:50 -0700

    create StandController and StandRepository

---
+++ letsbookit/src/main/java/com/libby/letsbookit/controller/StandController.java
@@ -0,0 +1,9 @@
+package com.libby.letsbookit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StandController {
+
+}
---
+++ letsbookit/src/main/java/com/libby/letsbookit/repositories/StandRepository.java
@@ -0,0 +1,9 @@
+package com.libby.letsbookit.repositories;
+
+import com.libby.letsbookit.model.Stand;
+import javax.transaction.Transactional;
+
+@Transactional
+public interface StandRepository {
+
+}
