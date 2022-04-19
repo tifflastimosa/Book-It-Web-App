7cc1ca62efa8f81cbecb9d37f263e3e94d567c72
moranandrew <moran.an@northeastern.edu>
2022-04-19 14:59:39 -0700

    added comments to StandRepository

--- letsbookit/src/main/java/com/libby/letsbookit/repositories/StandRepository.java
+++ letsbookit/src/main/java/com/libby/letsbookit/repositories/StandRepository.java
@@ -4,6 +4,9 @@ import com.libby.letsbookit.model.Stand;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for class Stand Repository.
+ */
 @Repository
 public interface StandRepository extends CrudRepository<Stand, Integer> {
 
