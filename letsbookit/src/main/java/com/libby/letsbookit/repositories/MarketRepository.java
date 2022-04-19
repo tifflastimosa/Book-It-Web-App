294fb1c8cfe7d3356d3563ce488600bfee73601e
moranandrew <moran.an@northeastern.edu>
2022-04-18 22:01:41 -0700

    created MarketRepository

---
+++ letsbookit/src/main/java/com/libby/letsbookit/repositories/MarketRepository.java
@@ -0,0 +1,10 @@
+package com.libby.letsbookit.repositories;
+
+import com.libby.letsbookit.model.Market;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarketRepository extends CrudRepository<Market, Integer> {
+
+}
