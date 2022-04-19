a9933bde3c336ff05c9f9a34b4078dfa1475f356
moranandrew <moran.an@northeastern.edu>
2022-04-19 12:12:27 -0700

    added comments to MarketRepository

--- letsbookit/src/main/java/com/libby/letsbookit/repositories/MarketRepository.java
+++ letsbookit/src/main/java/com/libby/letsbookit/repositories/MarketRepository.java
@@ -4,6 +4,9 @@ import com.libby.letsbookit.model.Market;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for class Market Repository.
+ */
 @Repository
 public interface MarketRepository extends CrudRepository<Market, Integer> {
 
