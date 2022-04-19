0a06a10643c5fb80a8d56def48e3c7824fa88159
moranandrew <moran.an@northeastern.edu>
2022-04-19 11:49:27 -0700

    added comments to MarketController

--- letsbookit/src/main/java/com/libby/letsbookit/controller/MarketController.java
+++ letsbookit/src/main/java/com/libby/letsbookit/controller/MarketController.java
@@ -11,14 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * MarketController class provides CRUD requests for the client to interact with the database.
+ */
+// DO I NEED @CROSSORIGIN?
 @RestController
 @RequestMapping("/markets")
 public class MarketController {
 
+  // connect with the service layer
   @Autowired
   private MarketService marketService;
 
+  // POST Request
 
+  /**
+   * Allows the client to request to create a Market, which represents a market organization, that
+   * will be added to the database.
+   *
+   * @param name the name of the market.
+   * @param description the description of the market.
+   * @param frequency the frequency that the market is held.
+   * @param contactNumber the contact phone number for the market.
+   * @param contactEmail the contact email for the market.
+   * @param website the website URL for the market.
+   * @param socialMedia the social media link for the market.
+   * @return Returns HTTP status, if the request is good or bad, and also returns the id.
+   */
   @PostMapping(value = "/create")
   public ResponseEntity<Integer> createMarket(@RequestParam(value = "name") String name,
                                               @RequestParam(value = "description") String description,
