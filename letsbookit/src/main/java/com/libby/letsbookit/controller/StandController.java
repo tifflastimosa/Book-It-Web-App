a2c57ad9651f07d555a71351bcd9c102fb38b342
Tiffany <lastimosa.ti@northeastern.edu>
2022-05-01 00:12:15 -0700

    update stand

--- letsbookit/src/main/java/com/libby/letsbookit/controller/StandController.java
+++ letsbookit/src/main/java/com/libby/letsbookit/controller/StandController.java
@@ -5,6 +5,7 @@ import com.libby.letsbookit.service.EventService;
 import com.libby.letsbookit.service.StandService;
 import com.libby.letsbookit.service.VendorService;
 import java.util.List;
+import javax.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,24 +28,19 @@ public class StandController {
   // connect with the service layer
   @Autowired
   private StandService standService;
+
+  @Autowired
   private EventService eventService;
+
+  @Autowired
   private VendorService vendorService;
 
   // POST Request
 
-  @PostMapping(value = "/create")
-  public ResponseEntity<Integer> createStand(@PathVariable("id") Integer id,
-                                            @RequestParam(value = "name") String name,
-                                            @RequestParam(value = "notes") String notes,
-                                            @RequestParam(value = "booked") Boolean booked,
-                                            @RequestParam(value = "price") Float price,
-                                            @RequestParam(value = "eventId") Integer eventId,
-                                            @RequestParam(value = "vendorId", required = false) Integer vendorId) {
+  @PostMapping(value = "/add-stand")
+  public ResponseEntity<Stand> addStand(@RequestBody Stand stand) {
     try {
-      return new ResponseEntity<>(this.standService.updateStand(id, name, notes, booked, price,
-                                                                this.eventService.getById(eventId),
-                                                                this.vendorService.findVendorById(vendorId)),
-                                                                HttpStatus.OK);
+      return new ResponseEntity<>(this.standService.addStand(stand), HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
@@ -51,9 +48,14 @@ public class StandController {
 
   // GET Requests
 
-  @GetMapping
+  /**
+   * Client request to get all stands from the stands table in the database.
+   *
+   * @return A list of all stands in the database.
+   */
+  @GetMapping(value = "/all")
   public ResponseEntity<List<Stand>> getAllStands() {
-    List<Stand> stands = this.standService.findAllStands();
+    List<Stand> stands = this.standService.getAllStands();
     if (!stands.isEmpty()) {
       return new ResponseEntity<>(stands, HttpStatus.OK);
     } else {
@@ -61,9 +63,15 @@ public class StandController {
     }
   }
 
+  /**
+   * Client request to get a stand given an id from the database.
+   *
+   * @param id The unique id/primary key of the stand.
+   * @return The stand of the given id number.
+   */
   @GetMapping(value = "/{id}")
   public ResponseEntity<Stand> getStand(@PathVariable Integer id) {
-    Stand stand = this.standService.findStandById(id);
+    Stand stand = this.standService.getStandyById(id);
     if (stand != null) {
       return new ResponseEntity<>(stand, HttpStatus.OK);
     } else {
@@ -73,18 +81,17 @@ public class StandController {
 
   // PUT Request
 
+  /**
+   * Client
+   * @param id
+   * @param stand
+   * @return
+   */
   @PutMapping(value = "/update/{id}")
-  public ResponseEntity<Integer> updateStand(@PathVariable("id") Integer id,
-      @RequestParam(value = "name") String name,
-      @RequestParam(value = "notes") String notes,
-      @RequestParam(value = "booked") Boolean booked,
-      @RequestParam(value = "price") Float price,
-      @RequestParam(value = "eventId") Integer eventId,
-      @RequestParam(value = "vendorId", required = false) Integer vendorId) {
+  public ResponseEntity<Stand> updateStand(@PathVariable("id") @Positive Integer id,
+                                           @RequestBody Stand stand) {
     try {
-      return new ResponseEntity<>(this.standService.updateStand(id, name, notes, booked, price,
-                                  this.eventService.getById(eventId),
-                                  this.vendorService.findVendorById(vendorId)), HttpStatus.OK);
+      return new ResponseEntity<>(this.standService.updateStand(id, stand), HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
@@ -93,7 +100,7 @@ public class StandController {
   // DELETE Request
 
   /**
-   * Deletes a stand from the database when given an id.
+   * Client request to delete a stand from the database when given an id.
    *
    * @param id the primary key, unique id of the stand.
    * @return Returns the HTTP status, if the request is good or bad.
@@ -107,4 +114,5 @@ public class StandController {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
       }
+
 }
